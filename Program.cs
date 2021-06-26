fe684986f2aa8b00b454c8b2db5f0db70b100a32
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 22:52:30 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercicio_condicional_Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+	
+	
+	 {
+		int idadePessoa;
+		Console.WriteLine("Qual é a sua idade?");
+		idadePessoa = int.Parse(Console.ReadLine());
+		
+		if(idadePessoa < 5 ){
+		    Console.WriteLine("você não é classificável");
+		}
+		
+		else{
+		    if(idadePessoa <= 7){
+		        Console.WriteLine("Sua categoria é: Infantil A");
+		    }
+		    
+		    else{
+		        if(idadePessoa < 10 ){
+		        Console.WriteLine("Sua categoria é: Infantil B");
+		    }
+		
+		        else{
+		            if(idadePessoa <= 13){
+		            Console.WriteLine("Sua categoria é: Juvenil A");
+		        }
+		            else{
+		                if(idadePessoa <= 17){
+		                Console.WriteLine("Sua categoria é: Juvenil B");
+		            
+		        }
+		        
+		                else {
+		                    Console.WriteLine("Sua categoria é: Sênior");
+		            }
+		        }
+		    }
+		    
+		        
+		    }
+		}
+	}
+}
+
+}
+        }
+    }
+
